0956e070f8f5cb674df2077996e75eef9ce5f289
xonmov <86357461+xonmov@users.noreply.github.com>
2026-03-26 17:17:00 +0530

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,22 @@
 #include <WiFi.h>
+#include "ota.h"
+
+const char* ssid = "ESP_TEST";
+const char* password = "12345678";
 
 void setup() {
   Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  Serial.println(WiFi.localIP());
+
+  setupOTA();   // 🔥 just one line
 }
 
 void loop() {
-  Serial.println("Hello from Naveen");
-  delay(2000);
+  handleOTA();  // 🔥 just one line
+
+  // your project code here
 }
