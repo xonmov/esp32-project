25f06222b1545efdb1749e949a84405b05fe42b5
xonmov <86357461+xonmov@users.noreply.github.com>
2026-03-29 15:32:19 +0530

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -4,10 +4,8 @@
 const char* ssid = "POCO";
 const char* password = "123456789";
 
-const int motorPin = D0;   // Motor connected to D0
-const int pwmChannel = 0;
-const int pwmFreq = 10000; // 10 kHz
-const int pwmResolution = 8; // 0–255
+const int motorPin = D0;
+
 
 
 void setup() {
@@ -20,8 +18,8 @@ void setup() {
 
   setupOTA();   // 🔥 just one line
 
-  ledcSetup(pwmChannel, pwmFreq, pwmResolution);
-  ledcAttachPin(motorPin, pwmChannel);
+  ledcAttach(motorPin, 10000, 8); // pin, frequency, resolution
+}
 
 }
 
@@ -30,17 +28,7 @@ void loop() {
 delay(1000);
  Serial.println("ok");
   
-// Slow increase
-  for (int duty = 0; duty <= 255; duty++) {
-    ledcWrite(pwmChannel, duty);
-    delay(20);
-  }
-
-  // Slow decrease
-  for (int duty = 255; duty >= 0; duty--) {
-    ledcWrite(pwmChannel, duty);
-    delay(20);
-  }
+ledcWrite(motorPin, 200); // constant speed
 
 
 }
